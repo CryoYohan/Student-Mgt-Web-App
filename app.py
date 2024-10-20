097cd944b2b8032c32c16dd6feeb4a29d503b83b
itsmecyril <cyrilypil@gmail.com>
2024-10-21 01:06:08 +0800

    register done, login registered accountns, connected database

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
 from flask import Flask,render_template,request,url_for,redirect,flash
+from dbhelper import Databasehelper
+from student import Student
+from hashpw import PasswordHashing
 
 app = Flask(__name__)
+db = Databasehelper()
+pwhash = PasswordHashing()
 app.secret_key="!@#"
 
-students:list = [
-    {'idno':'0001','lastname':'durano','firstname':'dennis','course':'bscpe','level':'4',},
-    {'idno':'0002','lastname':'india','firstname':'alpha','course':'bscs','level':'2',},
-    {'idno':'0003','lastname':'hotel','firstname':'bravo','course':'bsit','level':'4',},
-    {'idno':'0004','lastname':'golf','firstname':'delta','course':'bsba','level':'3',},
-    {'idno':'0005','lastname':'foxtrot','firstname':'echo','course':'bscs','level':'3',},
-]
 
 @app.route("/userregister",methods=['POST'])
 def userregister()->None:
-    return render_template('register.html')
+    idno:str = request.form['idno']
+    lastname:str = request.form['lastname']
+    middlename:str = request.form['midinit']
+    firstname:str = request.form['firstname']
+    course:str = request.form['course']
+    level:int = request.form['level']
+    username:str = request.form['username']
+    password:str = request.form['password']
+    confirm_password:str = request.form['confirmpassword']
+    if not idno or not lastname or not middlename or not firstname or not course or not level or not username or not password or not confirm_password:
+        flash("Please fill all fields!", "warning")
+        return render_template('register.html')
+
+    if password != confirm_password:
+        flash("Passwords do not match!", "warning")
+        return render_template('register.html')
+
+    flash("Registered successfully!", "info")
+    #student = Student(idno=idno,lastname=lastname,firstname=firstname,midinit=middlename,course=course,level=level,password_plain=password)
+    hashed_pw = pwhash.hashpassword(password)
+    db.add_students(idno=idno,lastname=lastname,firstname=firstname,midinit=middlename,course=course,level=level,username=username,password_plain=password, password_hash=hashed_pw)
+    return redirect(url_for('login'))
 
 
 @app.route("/userlogin",methods=['POST'])
 def userlogin()->None:
     username:str = request.form['username']
     password:str = request.form['password']
-    if username == "admin" and password=="user":
-        flash("LOGIN SUCCESSFUL!")
-        return redirect(url_for("landingpage"))
-    else:
-        flash("LOGIN FAILED!")
-        return redirect(url_for("login"))
-
+    students = db.getall_students()
+    for student in students:
+        if student['username'] == username and student['password_plain'] == password:
+            flash("LOGIN SUCCESSFUL!")
+            return redirect(url_for("landingpage"))
+    flash("LOGIN FAILED!")
+    return redirect(url_for("login"))
+            
+               
 @app.route("/register")
 def register():
     return render_template("register.html", pagetitle="Register",shownavbar=False)
@@ -42,7 +63,7 @@ def landingpage():
 
 @app.route("/showstudents")
 def show()->None:
-    return render_template("show.html",slist = students,pagetitle='student list', shownavbar=True)
+    return render_template("show.html",slist = db.getall_students(),pagetitle='student list', shownavbar=True)
 
 
 if __name__=="__main__":
--- dbhelper.py
+++ dbhelper.py
@@ -1,25 +1,56 @@
-import mysql.connector
+import mysql.connector as mariadb
 class Databasehelper:
     def __init__(self)->None:
         self.host = 'localhost'
-        self.user = 'root@localhost'
+        self.user = 'root'
         self.password = ''
         self.database = 'users'
+        self.table = "students"
 
     def getdb_connection(self):
-        connection = mysql.connector.connect(
+        connection = mariadb.connect(
             host= self.host,
             user= self.user,
             password= self.password,
             database= self.database
         )
         return connection
-
-    def getall_users(self, query:str):
+    
+    def getprocess(self,sql:str):
         connection = self.getdb_connection()
         cursor = connection.cursor(dictionary=True)
-        cursor.execute(query)
-        users = cursor.fetchall()
+        cursor.execute(sql)
+        data:list = cursor.fetchall()
         cursor.close()
         connection.close()
+        return data
+
+    def postprocess(self,sql:str):
+        connection = self.getdb_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql)
+        connection.commit()
+        cursor.close()
+        connection.close() 
+
+    def getall_students(self)->list:
+        query = f"SELECT * FROM {self.table}"
+        users:list = self.getprocess(query)
         return users
+    
+    def find_users(self,idno:str):
+        connection = self.getdb_connection()
+        cursor:object = connection.cursor(dictionary=True)
+        cursor.execute(f"SELECT * FROM users WHERE `idno` = {idno}")
+        data:list = cursor.fetchall()
+        connection.commit()
+        cursor.close()
+        connection.close()
+    
+    def add_students(self,**kwargs):
+        keys:list = kwargs.keys()
+        values:list=kwargs.values()
+        columns:str = ",".join(keys)
+        formatted_values = ",".join([f"'{v}'" if isinstance(v, str) else str(v) for v in values])
+        sql:str = f"INSERT INTO {self.table} ({columns}) VALUES({formatted_values})"
+        return self.postprocess(sql)
